a56a00faa4ef1fecba4ad32a1e4391203e98a833
Bhavesh Gupta <bhaveshsgupta@gmail.com>
2014-08-07 14:34:48 +0530

    new

--- src/domreader/DOMReader.java
+++ src/domreader/DOMReader.java
@@ -6,7 +6,6 @@ package domreader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,21 +15,39 @@ import org.xml.sax.SAXException;
 
 /**
  *
- * @author Bhavesh
+ * @author Bhavesh Gupta
  */
 public class DOMReader {
 
     /**
      * @param args the command line arguments
+     * @throws javax.xml.parsers.ParserConfigurationException
+     * @throws java.io.IOException
+     * @throws org.xml.sax.SAXException
      */
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-    File xmlFile = new File("Students.xml");
+    File xmlFile = new File("CreatedXML.xml");
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     DocumentBuilder builder = factory.newDocumentBuilder();
     Document doc = builder.parse(xmlFile);
     System.out.println(doc.getDocumentElement().getNodeName());
+    String root = doc.getDocumentElement().getNodeName();
+    System.out.println(root);    
+    NodeList abh = doc.getElementsByTagName("book");
+        
+    /*
+    NodeList list = doc.getElementsByTagName("book");
     NodeList doclist = (NodeList) doc.getElementsByTagName("student");
-    
-    
+    for(int i=0;i<list.getLength();i++)
+        {
+        Node book = list.item(i);
+    if(book.getNodeType()== Node.ELEMENT_NODE){
+    Element st = (Element) book;
+    System.out.println();
+    }
+        
+        }
+    */
     }
 }
